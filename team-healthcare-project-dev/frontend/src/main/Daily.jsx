5c4041edf43180e91da4f23895eefb0fa3bbc634
lys-17 <lkl10055@naver.com>
2025-07-08 13:39:17 +0900

    page

---
+++ team-healthcare-project-dev/frontend/src/main/Daily.jsx
@@ -0,0 +1,943 @@
+/* 일정 관리 */
+
+position: relative;
+width: 1920px;
+height: 1080px;
+
+background: linear-gradient(0deg, #FFFFFF, #FFFFFF);
+
+
+/* backboard */
+
+position: absolute;
+width: 1920px;
+height: 1080px;
+left: 0px;
+top: 0px;
+
+background: linear-gradient(180deg, rgba(0, 124, 255, 0.2) 0%, rgba(0, 188, 255, 0.2) 100%);
+
+
+/* main board */
+
+position: absolute;
+width: 1820px;
+height: 1000px;
+left: calc(50% - 1820px/2);
+top: calc(50% - 1000px/2);
+
+background: #FFFFFF;
+border-radius: 10px;
+
+
+/* menu */
+
+position: absolute;
+width: 237px;
+height: 1000px;
+left: 0px;
+top: 40px;
+
+
+
+/* menu board */
+
+position: absolute;
+width: 237px;
+height: 1000px;
+left: 0px;
+top: 40px;
+
+background: #007CFF;
+border-radius: 0px 40px 40px 0px;
+
+
+/* 대시보드 */
+
+position: absolute;
+width: 136px;
+height: 26.32px;
+left: 38px;
+top: 206.45px;
+
+font-family: 'GyeonggiTitle';
+font-style: normal;
+font-weight: 500;
+font-size: 25px;
+line-height: 25px;
+
+color: #FFFFFF;
+
+
+
+/* 안전 모니터링 */
+
+position: absolute;
+width: 136px;
+height: 26.32px;
+left: 38px;
+top: 310.4px;
+
+font-family: 'GyeonggiTitle';
+font-style: normal;
+font-weight: 500;
+font-size: 25px;
+line-height: 25px;
+
+color: #FFFFFF;
+
+
+
+/* 보호 대상자 */
+
+position: absolute;
+width: 116px;
+height: 26.32px;
+left: 38px;
+top: 258.4px;
+
+font-family: 'GyeonggiTitle';
+font-style: normal;
+font-weight: 500;
+font-size: 25px;
+line-height: 25px;
+
+color: #FFFFFF;
+
+
+
+/* 알림 센터 */
+
+position: absolute;
+width: 94px;
+height: 26.32px;
+left: 38px;
+top: 362.4px;
+
+font-family: 'GyeonggiTitle';
+font-style: normal;
+font-weight: 500;
+font-size: 25px;
+line-height: 25px;
+
+color: #FFFFFF;
+
+
+
+/* 일정 관리 */
+
+position: absolute;
+width: 116px;
+height: 26.32px;
+left: 38px;
+top: 414.4px;
+
+font-family: 'GyeonggiTitle';
+font-style: normal;
+font-weight: 500;
+font-size: 25px;
+line-height: 25px;
+
+color: #FFFFFF;
+
+
+
+/* 메시지 */
+
+position: absolute;
+width: 92px;
+height: 26.32px;
+left: 38px;
+top: 466.4px;
+
+font-family: 'GyeonggiTitle';
+font-style: normal;
+font-weight: 500;
+font-size: 25px;
+line-height: 25px;
+
+color: #FFFFFF;
+
+
+
+/* user 1 */
+
+position: absolute;
+width: 50.27px;
+height: 50.27px;
+left: 34.36px;
+top: 105px;
+
+background: url(user.png);
+
+
+/* logout */
+
+position: absolute;
+width: 151.84px;
+height: 34.88px;
+left: 36.08px;
+top: 935.19px;
+
+
+
+/* logout 1 */
+
+position: absolute;
+width: 33.86px;
+height: 34.88px;
+left: 36.08px;
+top: 935.19px;
+
+background: url(logout.png);
+
+
+/* 로그아웃 */
+
+position: absolute;
+width: 104.65px;
+height: 32.83px;
+left: 83.27px;
+top: 936.22px;
+
+font-family: 'NanumHuman OTF';
+font-style: normal;
+font-weight: 800;
+font-size: 20px;
+line-height: 26px;
+
+color: #FFFFFF;
+
+
+
+/* setting */
+
+position: absolute;
+width: 45.14px;
+height: 45.14px;
+left: 26.43px;
+top: 870.06px;
+
+background: url(설정.png);
+
+
+/* 일정 관리 */
+
+position: absolute;
+width: 259px;
+height: 84px;
+left: 327px;
+top: 100px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 70px;
+line-height: 84px;
+/* 상자 높이와 동일 */
+
+color: #0869CC;
+
+
+
+/* 캘린더 위치 */
+
+position: absolute;
+width: 700px;
+height: 420px;
+left: 327px;
+top: 219px;
+
+
+
+/* Rectangle 60 */
+
+position: absolute;
+width: 700px;
+height: 420px;
+left: 327px;
+top: 219px;
+
+background: #D9D9D9;
+
+
+/* 캘린더 */
+
+position: absolute;
+width: 63px;
+height: 24.36px;
+left: 513px;
+top: 363.48px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 29px;
+
+color: #000000;
+
+
+
+/* 활동 기록 박스 */
+
+position: absolute;
+width: 651px;
+height: 750px;
+left: 1129px;
+top: 220px;
+
+
+
+/* 활동 기록 박스 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 651px;
+height: 750px;
+left: 1129px;
+top: 220px;
+
+background: #FDFDFD;
+border: 1px solid #D9D9D9;
+box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.25);
+border-radius: 13px;
+
+
+/* 대상자 이름 */
+
+position: absolute;
+width: 141px;
+height: 77px;
+left: 1180px;
+top: 263px;
+
+
+
+/* 000 */
+
+position: absolute;
+width: 107px;
+height: 77px;
+left: 1180px;
+top: 263px;
+
+font-family: 'NanumHuman OTF';
+font-style: normal;
+font-weight: 700;
+font-size: 60px;
+line-height: 77px;
+/* 상자 높이와 동일 */
+
+color: #00458B;
+
+
+
+/* 님 */
+
+position: absolute;
+width: 26px;
+height: 36px;
+left: 1295px;
+top: 295px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 30px;
+line-height: 36px;
+/* 상자 높이와 동일 */
+
+color: #000000;
+
+
+
+/* 00.00.00 */
+
+position: absolute;
+width: 124px;
+height: 36px;
+left: 1180px;
+top: 340px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 500;
+font-size: 30px;
+line-height: 36px;
+/* 상자 높이와 동일 */
+
+color: #000000;
+
+
+
+/* 저장 버튼 */
+
+position: absolute;
+width: 70px;
+height: 35px;
+left: 1649px;
+top: 306px;
+
+
+
+/* 저장 버튼 */
+
+position: absolute;
+width: 70px;
+height: 35px;
+left: 1649px;
+top: 306px;
+
+background: #00458B;
+border-radius: 10px;
+
+
+/* 저장 */
+
+position: absolute;
+width: 26px;
+height: 18px;
+left: 1671px;
+top: 315px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 500;
+font-size: 15px;
+line-height: 18px;
+/* 상자 높이와 동일 */
+
+color: #FFFFFF;
+
+
+
+/* 일일 특이사항 */
+
+position: absolute;
+width: 539px;
+height: 166px;
+left: 1180px;
+top: 764px;
+
+
+
+/* 특이사항 박스 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 539px;
+height: 130px;
+left: 1180px;
+top: 800px;
+
+background: #FFFFFF;
+border: 1px solid #989898;
+
+
+/* 일일 특이사항 */
+
+position: absolute;
+width: 136px;
+height: 30px;
+left: 1180px;
+top: 764px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 25px;
+line-height: 30px;
+/* 상자 높이와 동일 */
+
+color: #000000;
+
+
+
+/* 구분선 */
+
+position: absolute;
+width: 539px;
+height: 1px;
+left: 1180px;
+top: 741px;
+
+background: #CDCDCD;
+
+
+/* 구분선 */
+
+position: absolute;
+width: 539px;
+height: 1px;
+left: 1180px;
+top: 637px;
+
+background: #CDCDCD;
+
+
+/* 수면 상태 */
+
+position: absolute;
+width: 274px;
+height: 63px;
+left: 1180px;
+top: 658px;
+
+
+
+/* 수면 상태 */
+
+position: absolute;
+width: 93px;
+height: 30px;
+left: 1180px;
+top: 658px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 25px;
+line-height: 30px;
+/* 상자 높이와 동일 */
+
+color: #000000;
+
+
+
+/* Group 5 */
+
+position: absolute;
+width: 274px;
+height: 24px;
+left: 1180px;
+top: 697px;
+
+
+
+/* 좋음 */
+
+position: absolute;
+width: 65px;
+height: 24px;
+left: 1180px;
+top: 697px;
+
+
+
+/* 좋음 */
+
+position: absolute;
+width: 35px;
+height: 24px;
+left: 1210px;
+top: 697px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 24px;
+/* 상자 높이와 동일 */
+
+color: #000000;
+
+
+
+/* 체크 박스 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 20px;
+height: 20px;
+left: 1180px;
+top: 699px;
+
+background: #FFFFFF;
+border: 1px solid #000000;
+
+
+/* 보통 */
+
+position: absolute;
+width: 65px;
+height: 24px;
+left: 1286px;
+top: 697px;
+
+
+
+/* 보통 */
+
+position: absolute;
+width: 35px;
+height: 24px;
+left: 1316px;
+top: 697px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 24px;
+/* 상자 높이와 동일 */
+
+color: #000000;
+
+
+
+/* 체크 박스 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 20px;
+height: 20px;
+left: 1286px;
+top: 699px;
+
+background: #FFFFFF;
+border: 1px solid #000000;
+
+
+/* 나쁨 */
+
+position: absolute;
+width: 65px;
+height: 24px;
+left: 1389px;
+top: 697px;
+
+
+
+/* 나쁨 */
+
+position: absolute;
+width: 35px;
+height: 24px;
+left: 1419px;
+top: 697px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 24px;
+/* 상자 높이와 동일 */
+
+color: #000000;
+
+
+
+/* 체크 박스 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 20px;
+height: 20px;
+left: 1389px;
+top: 699px;
+
+background: #FFFFFF;
+border: 1px solid #000000;
+
+
+/* 항목 체크란 */
+
+position: absolute;
+width: 539px;
+height: 218px;
+left: 1180px;
+top: 399px;
+
+
+
+/* 항목 박스 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 539px;
+height: 51px;
+left: 1180px;
+top: 399px;
+
+background: #FFFFFF;
+border: 1px solid #989898;
+
+
+/* 항목에 대한 체크란 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 539px;
+height: 167px;
+left: 1180px;
+top: 450px;
+
+background: #FFFFFF;
+border: 1px solid #989898;
+
+
+/* 식사 횟수 */
+
+position: absolute;
+width: 93px;
+height: 30px;
+left: 1194px;
+top: 410px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 25px;
+line-height: 30px;
+/* 상자 높이와 동일 */
+
+color: #000000;
+
+
+
+/* 아침 체크 박스 */
+
+position: absolute;
+width: 68px;
+height: 24px;
+left: 1200px;
+top: 478px;
+
+
+
+/* Rectangle 71 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 20px;
+height: 20px;
+left: 1200px;
+top: 480px;
+
+background: #FFFFFF;
+border: 1px solid #000000;
+
+
+/* 아침 */
+
+position: absolute;
+width: 35px;
+height: 24px;
+left: 1233px;
+top: 478px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 24px;
+/* 상자 높이와 동일 */
+
+color: #000000;
+
+
+
+/* 점심 체크 박스 */
+
+position: absolute;
+width: 68px;
+height: 24px;
+left: 1200px;
+top: 525px;
+
+
+
+/* Rectangle 72 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 20px;
+height: 20px;
+left: 1200px;
+top: 527px;
+
+background: #FFFFFF;
+border: 1px solid #000000;
+
+
+/* 점심 */
+
+position: absolute;
+width: 35px;
+height: 24px;
+left: 1233px;
+top: 525px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 24px;
+/* 상자 높이와 동일 */
+
+color: #000000;
+
+
+
+/* 저녁 체크 박스 */
+
+position: absolute;
+width: 68px;
+height: 24px;
+left: 1200px;
+top: 567px;
+
+
+
+/* Rectangle 73 */
+
+box-sizing: border-box;
+
+position: absolute;
+width: 20px;
+height: 20px;
+left: 1200px;
+top: 569px;
+
+background: #FFFFFF;
+border: 1px solid #000000;
+
+
+/* 저녁 */
+
+position: absolute;
+width: 35px;
+height: 24px;
+left: 1233px;
+top: 567px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 24px;
+/* 상자 높이와 동일 */
+
+color: #000000;
+
+
+
+/* 보호 대상자 박스 */
+
+position: absolute;
+width: 700px;
+height: 50px;
+left: 327px;
+top: 666px;
+
+background: #0869CC;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* 굵은 선 */
+
+position: absolute;
+width: 700px;
+height: 3px;
+left: 327px;
+top: 967px;
+
+background: #0869CC;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* 선 */
+
+position: absolute;
+width: 700px;
+height: 1px;
+left: 327px;
+top: 817px;
+
+background: #0869CC;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* 선 */
+
+position: absolute;
+width: 700px;
+height: 1px;
+left: 327px;
+top: 867px;
+
+background: #0869CC;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* 선 */
+
+position: absolute;
+width: 700px;
+height: 1px;
+left: 327px;
+top: 917px;
+
+background: #0869CC;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* 선 */
+
+position: absolute;
+width: 700px;
+height: 1px;
+left: 327px;
+top: 766px;
+
+background: #0869CC;
+transform: matrix(1, 0, 0, -1, 0, 0);
+
+
+/* 보호 대상자 */
+
+position: absolute;
+width: 302px;
+height: 30px;
+left: calc(50% - 302px/2 - 283px);
+top: 676px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 25px;
+line-height: 30px;
+/* 상자 높이와 동일 */
+letter-spacing: 1.5em;
+
+color: #FFFFFF;
+
+
+
+/* + 항목 추가 */
+
+position: absolute;
+width: 69px;
+height: 18px;
+left: 1650px;
+top: 376px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 700;
+font-size: 15px;
+line-height: 18px;
+/* 상자 높이와 동일 */
+
+color: #0869CC;
+
